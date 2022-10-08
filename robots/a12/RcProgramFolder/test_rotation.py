f92668107c1140961bde4112c7f8724027b3538d
MagicWinnie <magicwinnie@yandex.ru>
2022-10-08 13:32:14 +0300

    Project restructured

--- robots/a12/RcProgramFolder/test_rotation.py
+++
@@ -1,8 +0,0 @@
-joint_ptp("home",100.0,100.0,0.0)
-for _ in range(5):
-    joint_ptp("bottle_rotate_1", 100.0, 100.0, 5)
-    joint_ptp("bottle_rotate_2", 100.0, 100.0, 5)
-    joint_ptp("bottle_rotate_3", 100.0, 100.0, 5)
-    joint_ptp("bottle_rotate_4", 100.0, 100.0, 5)
-    joint_ptp("bottle_rotate_1", 100.0, 100.0, 5)
-joint_ptp("home",100.0,100.0,0.0)
