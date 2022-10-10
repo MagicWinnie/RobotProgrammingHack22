94014b6f8e7919439710370c42a3e339c7424f1b
MagicWinnie <magicwinnie@yandex.ru>
2022-10-10 15:48:16 +0300

    Renamed project; Added delays;

--- robots/a12/RcProgramFolder/task1.py
+++ robots/a12/RcProgramFolder/task1.py
@@ -1,15 +1,20 @@
+import time
+GRAB_DELAY = 100 # ms
+POUR_DELAY = 100 # ms
 set_do("grab",False)
 joint_ptp("home",100.0,100.0,0.0)
+start_time = time.time() # start counting time only when robot is moving from home pos
 ### GET TUBE ###
 joint_ptp("tube_1_up",100.0,100.0,0.0)
 cart_ptp("tube_1_down",100.0,100.0,0.0,"Flange")
 set_do("grab",True)
+wait_msec(GRAB_DELAY)
 line("tube_1_up_xyz",200.0,1000.0,0.0,"Flange")
 ### POUR TUBE 1 ###
 joint_ptp("ver2_1", 100.0, 100.0, 4)
 joint_ptp("ver2_2", 100.0, 100.0, 4)
 joint_ptp("ver2_3", 100.0, 100.0, 4)
-wait_msec(200)
+wait_msec(POUR_DELAY)
 joint_ptp("ver2_2", 100.0, 100.0, 4)
 ### PUT TUBE 1 ###
 joint_ptp("tube_1_up",100.0,100.0,0.0)
@@ -20,12 +25,13 @@ cart_ptp("tube_1_up_xyz",100.0,100.0,0.0,"Flange")
 joint_ptp("tube_2_up", 100.0, 100.0, 0.0)
 cart_ptp("tube_2_down", 100.0, 100.0, 0.0, "Flange")
 set_do("grab",True)
+wait_msec(GRAB_DELAY)
 line("tube_2_up_xyz",200.0,1000.0,0.0,"Flange")
 ### POUR TUBE 2 ###
 joint_ptp("ver2_1", 100.0, 100.0, 4)
 joint_ptp("ver2_2", 100.0, 100.0, 4)
 joint_ptp("ver2_3", 100.0, 100.0, 4)
-wait_msec(200)
+wait_msec(POUR_DELAY)
 joint_ptp("ver2_2", 100.0, 100.0, 4)
 ### PUT TUBE 2 ###
 joint_ptp("tube_2_up", 100.0, 100.0, 0.0)
@@ -36,6 +42,7 @@ cart_ptp("tube_2_up_xyz", 100.0, 100.0, 0.0, "Flange")
 joint_ptp("bottle_up",100.0,100.0,0.0)
 cart_ptp("bottle_down",100.0,100.0,0.0,"Flange")
 set_do("grab",True)
+wait_msec(GRAB_DELAY)
 cart_ptp("bottle_up_xyz", 100.0, 100.0, 15, "Flange")
 ### MIX BOTTLE ###
 joint_ptp("bottle_rotate_1", 100.0, 100.0, 10)
@@ -50,3 +57,5 @@ cart_ptp("bottle_down",100.0,100.0,0.0,"Flange")
 set_do("grab",False)
 cart_ptp("bottle_up_xyz", 100.0, 100.0, 10, "Flange")
 joint_ptp("home", 100.0, 100.0, 15)
+end_time = time.time() - start_time
+notify(round(end_time, 3),"Execution time")
